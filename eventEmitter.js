a1e5fb5d0aff88b8079e1c418774a0970f0e9a99
Toba-dean <tobaogundimu@gmail.com>
2022-07-25 14:47:19 +0100

    done.

--- eventEmitter.js
+++ eventEmitter.js
@@ -3,10 +3,13 @@ const eventEmitter = require('events');
 const customEmitter = new eventEmitter();
 
 customEmitter.on('response', (name, id) => {
-  console.log(`data fetched, for ${name} with id number ${id} `);
+  console.log(`data fetched, for ${name} with id number: ${id} `);
 })
-customEmitter.on('response', (name) => {
+customEmitter.on('response', name => {
   console.log(`fetching other data, ${name} `);
+})
+customEmitter.on('response', (_, id) => {
+  console.log(`The id for the file is ${id}`);
 })
 
 
--- streams.js
+++ streams.js
@@ -7,6 +7,7 @@ for(let i = 0; i <= 10000; i++) {
 // const { createReadStream } = require('fs')
 
 // const stream = createReadStream('./content/big.txt', { highWaterMark: 90000, encoding: 'utf-8' })
+
 // stream.on('data', result => {
 //   console.log(result);
 // })
